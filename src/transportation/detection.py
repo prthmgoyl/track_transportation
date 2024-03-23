f2e41e9e1b0fcbb92182119fe212c3003f555544
prthmgoyl <prthmgoyl@gmail.com>
2024-03-23 20:38:43 +0530

    Functionality

--- src/transportation/detection.py
+++ src/transportation/detection.py
@@ -3,6 +3,7 @@ import numpy as np
 from ultralytics import YOLO
 import cv2
 import math
+import tqdm
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
 
@@ -18,61 +19,78 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
-def detect_objects(img,stream=False , objects=None , box=False , segment=False , conf=0.3, text=True):
+def detect_objects(path,stream=True,csv=False,save=False , 
+                   objects=None , box=False , segment=False , conf=0.3, text=True):
+    
+    cap = cv2.VideoCapture(path)
 
+    #To save output
+    if save:
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter('output_video_path.mp4', fourcc, 25, (int(cap.get(3)), int(cap.get(4))))
+    
+    #Classes
     if objects is None:
         objects = classNames
-
-
-    results = model(img, stream=stream)
-    detections = np.empty((0, 5))
-
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
-
-            conff = math.ceil((box.conf[0] * 100)) / 100
-            cls = int(box.cls[0])
-            currentClass = classNames[cls]
-
-            if currentClass in objects and conff > conf:
-                currentArray = np.array([x1, y1, x2, y2, conff])
-                detections = np.vstack((detections, currentArray))
-                if text:
-                 cv2.putText(img, currentClass , (max(0, x1), max(35, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
     
-    if segment:
-        segmented_images = segment_objects(results)
-        for i, segmented_image in enumerate(segmented_images):
-            img = cv2.addWeighted(img, 1, segmented_image, 0.5, 0)
-            #cv2.putText(img, f'Segment {i}', (10, 35 * (i+1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
+    # CSV file setup
+    if csv:
+        csv_file_path = 'data.csv'
+        csv_columns = ['Frame Number', 'Object Type','Track ID']
 
-    if box:
-        bounding_boxes = detect_boxes(results,objects,conf)
-        for box in bounding_boxes:
-            x1, y1, x2, y2 = box['coordinates']
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            #cv2.putText(img, f"{box['class']} {box['confidence']}", (x1, max(35, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
-
-    return img
     
-# def detection(results):
-    
-#     return detections
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-def segment_objects(results): 
-    segmented_images = []
-    for r in results:
-        img = r.imgs[0]  # Extracting the image
-        masks = r.pred[0]['masks']  # Extracting masks from the prediction
-        for mask in masks:
-            # Applying the mask to the image
-            masked_image = cv2.bitwise_and(img, img, mask=mask.astype(np.uint8))
-            segmented_images.append(masked_image)
-    return segmented_images
+    for frame_number in tqdm(range(total_frames)):
+        success, img = cap.read()
+        if not success:
+            break
+
+        results = model(img, stream=stream)
+        detections = np.empty((0, 5))
+
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                w, h = x2 - x1, y2 - y1
+
+                conff = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                currentClass = classNames[cls]
+
+                if currentClass in objects and conff > conf:
+                    currentArray = np.array([x1, y1, x2, y2, conff])
+                    detections = np.vstack((detections, currentArray))
+                    if text:
+                     cv2.putText(img, currentClass , (max(0, x1), max(35, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
+        
+        # if segment:
+        #     segmented_images = segment_objects(results)
+        #     for i, segmented_image in enumerate(segmented_images):
+        #         img = cv2.addWeighted(img, 1, segmented_image, 0.5, 0)
+        #         #cv2.putText(img, f'Segment {i}', (10, 35 * (i+1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
+
+        if box:
+            bounding_boxes = detect_boxes(results,objects,conf)
+            for box in bounding_boxes:
+                x1, y1, x2, y2 = box['coordinates']
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                #cv2.putText(img, f"{box['class']} {box['confidence']}", (x1, max(35, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
+
+    
+
+# def segment_objects(results): 
+#     segmented_images = []
+#     for r in results:
+#         img = r.imgs[0]  # Extracting the image
+#         masks = r.pred[0]['masks']  # Extracting masks from the prediction
+#         for mask in masks:
+#             # Applying the mask to the image
+#             masked_image = cv2.bitwise_and(img, img, mask=mask.astype(np.uint8))
+#             segmented_images.append(masked_image)
+#     return segmented_images
 
 def detect_boxes(results,objects,conf):
     bounding_boxes = []
