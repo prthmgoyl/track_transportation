885327c916061ac51704c3a47eb3d631e123b007
prthmgoyl <prthmgoyl@gmail.com>
2024-03-28 07:42:42 +0530

    Tracking

--- src/transportation/detection.py
+++ src/transportation/detection.py
@@ -4,6 +4,22 @@ from ultralytics import YOLO
 import cv2
 import math
 import tqdm
+import cvzone
+import time
+from transportation.tracking import ObjectTracker
+
+previous_speed_ms = 0 
+
+tracker = ObjectTracker()
+
+totalCount = []
+object_paths = {} 
+vehicles_entering = {}  # Keep track of when vehicles enter area_3
+vehicles_elapsed_time = {}  # Keep track of elapsed time for each vehicle
+area_1 = [(735, 203), (540, 208),(260, 605), (980, 569)]
+area_2 = [(645, 575), (960, 559), (820, 350), (650, 350)]
+area_3 = [(650, 343), (655, 213), (545, 213), (450, 352)]
+
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
 
@@ -20,7 +36,7 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               ]
 
 def detect_objects(path,stream=True,csv=False,save=False , 
-                   objects=None , box=False , segment=False , conf=0.3, text=True):
+                   objects=None , box=False , segment=False , conf=0.3, text=True , track=False):
     
     cap = cv2.VideoCapture(path)
 
@@ -49,6 +65,8 @@ def detect_objects(path,stream=True,csv=False,save=False ,
         results = model(img, stream=stream)
         detections = np.empty((0, 5))
 
+       
+
         for r in results:
             boxes = r.boxes
             for box in boxes:
@@ -66,6 +84,70 @@ def detect_objects(path,stream=True,csv=False,save=False ,
                     if text:
                      cv2.putText(img, currentClass , (max(0, x1), max(35, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
         
+        
+
+        if track:
+          cv2.polylines(img, [np.array(area_1, np.int32)], True, (255,255,255), 2)
+          for area in [area_2, area_3]:
+            cv2.polylines(img, [np.array(area, np.int32)], True, (0,0,255), 1)
+
+          resultsTracker = tracker.update(detections)
+          for result in resultsTracker:
+            x1, y1, x2, y2, track_id = result
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+
+            cx, cy = x1 + w // 2, y1 + h // 2
+            cv2.circle(img, (cx, cy), 4, (0,0,0))
+            cvzone.cornerRect(img, (x1, y1, w, h), l=1, rt=1, colorR=(0,0,0))
+
+            # Store object coordinates for tracing path
+            if track_id not in object_paths:
+                object_paths[track_id] = []
+            object_paths[track_id].append((cx, cy))  # Assuming cx, cy are the coordinates of the object
+
+            # Draw path on image
+            if len(object_paths[track_id]) > 1:
+                for i in range(1, len(object_paths[track_id])):
+                    cv2.line(img, object_paths[track_id][i-1], object_paths[track_id][i], (0,0,255), thickness=3)
+
+            for area in [area_1, area_2, area_3]:
+                if cv2.pointPolygonTest(np.array(area, np.int32), (int(cx), int(cy)), False) >= 0:
+                    if track_id not in totalCount:
+                        vehicles_entering[track_id] = time.time()
+                        totalCount.append(track_id)
+
+                     
+                    # Calculate average speed
+                    if track_id in vehicles_entering and area != area_1:
+                        elapsed_time = time.time() - vehicles_entering[track_id]
+
+                        if elapsed_time != 0:  # Avoid division by zero
+                            if track_id not in vehicles_elapsed_time:
+                                vehicles_elapsed_time[track_id] = elapsed_time
+
+                            if track_id in vehicles_elapsed_time:
+                                elapsed_time = vehicles_elapsed_time[track_id]
+                                distance = 18  # meters
+                                a_speed_ms = distance / elapsed_time
+                                a_speed_kh = a_speed_ms * 3.6
+
+                                # Calculating acceleration
+                                if elapsed_time != 0:  # Avoid division by zero
+                                    acceleration = (a_speed_ms - previous_speed_ms) / elapsed_time
+                                else:
+                                    acceleration = 0
+
+                                previous_speed_ms = a_speed_ms
+
+                                # Print or display velocity and acceleration on the box
+                                velocity_text = f'Spd: {a_speed_kh:.1f} km/h'
+                                acceleration_text = f'Acc: {acceleration:.2f} m/s²'
+                                cv2.putText(img, velocity_text, (max(0, x1+w//4), max(20, y1-14)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
+                                cv2.putText(img, acceleration_text, (max(0, x1+w//4), max(20, y1-3)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA | cv2.FONT_ITALIC)
+
+
+
         # if segment:
         #     segmented_images = segment_objects(results)
         #     for i, segmented_image in enumerate(segmented_images):
--- src/transportation/tracking.py
+++ src/transportation/tracking.py
@@ -1,8 +1,28 @@
+# # tracking.py
+# import numpy as np
+# from transportation.sort import Sort
+
+# tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+# def track_objects(detections):
+#     return tracker.update(detections)
+
+
 # tracking.py
+
 import numpy as np
 from transportation.sort import Sort
 
-tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+class ObjectTracker:
+    def __init__(self, max_age=20, min_hits=3, iou_threshold=0.3):
+        self.tracker = Sort(max_age=max_age, min_hits=min_hits, iou_threshold=iou_threshold)
 
-def track_objects(detections):
-    return tracker.update(detections)
+    def track_objects(self, detections):
+        return self.tracker.update(detections)
+
+    # def run_example(self):
+
+    #     tracked_objects = self.track_objects(detections)
+
+    #     # Do something with the tracked objects, e.g., print them
+    #     print("Tracked objects:", tracked_objects)
